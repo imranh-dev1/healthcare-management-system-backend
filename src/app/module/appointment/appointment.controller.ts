a6fa7e67e957ede1e003be2b77421452b389aa85
Imran Hossain <imranhossen0586@gmail.com>
2026-08-20 00:12:56 +0600

    feat(appointment): implement book appointment functionality with controller and routes

---
+++ src/app/module/appointment/appointment.controller.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
+import httpStatus from "http-status";
+import { AppointmentServices } from "./appointment.service";
+
+const bookAppointment = catchAsync(async (req: Request, res: Response) => { 
+
+    const result = await AppointmentServices.bookAppointment()
+
+	sendResponse(res, {
+		statusCode: httpStatus.OK,
+		success: true,
+		message: "User Book appointment successfully....",
+		data: result,
+	});
+});
+
+export const AppointmentController = {
+    bookAppointment
+}
